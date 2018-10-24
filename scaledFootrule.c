8771786fa59613f8ec609a39e5209f996c6bb08e
nonchalantwhale <44258935+nonchalantwhale@users.noreply.github.com>
2018-10-24 14:20:21 +1100

    Update scaledFootrule.c

--- scaledFootrule.c
+++ scaledFootrule.c
@@ -2,310 +2,215 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include "StrDict.h"
+#include "SList.h"
+#include "SList.c"
+#include "StrDict.c"
+typedef struct _elem {
+    
+    char *name;
+    float *places;
+    int nPrefs;
+}elem;
 
-// k
-#define URL_length 10
-#define flag_length 20
-#define path_length 100
-#define MAX_Line 1000
+static double scaledFoot(char *page, int pos, StrDict *ranks, int nRanks, int cSize) {
 
-
-
-char ***T;
-// global 3d array i repersent T1,T2...,j,k
-double mini = 999;
-// i
-int T_number;
-// j
-int max_url;
-int union_length;
-
-void swap(char *str1, char *str2)
-{
-	char *temp = (char *)malloc((strlen(str1) + 1) * sizeof(char));
-	strcpy(temp, str1);
-	strcpy(str1, str2);
-	strcpy(str2, temp);
-	free(temp);
+    double sum = 0;
+    for (int i = 0; i < nRanks; i++) {
+    
+        int rank = lookup(ranks[i], page);  //T_i(c)
+        if (rank == -1) continue;    //not in this rank list
+        
+        sum += fabs((double)rank/(double)nEntries(ranks[i]) - (double)pos/(double)cSize);
+    }
+    return sum;
 }
 
-void permute(char **a, int l, int r)
-{
-	int i, j;
-	int b, c, d;
-	if (l == r) {
-		// base case
-		// start calculation
-		double total = 0;
-		for (j = 0; j <= r; j++) {
-			// printf("%s  ", a[j]);
-		}
-		//printf("\n");
-		for (b = 0; b < T_number;b++) {
-			int mylength = 0;
-			for (c = 0; c < max_url;c++) {
-				if (strcmp(T[b][c],"")==0) {
-					;
-				}
-				else {
-					mylength++;
-				}
-			}
-			// printf("mylenght is %d\n", mylength);
-			for (c = 0; c < max_url; c++) {
-				if (strcmp(T[b][c], "") == 0) {
-					;
-				}
-				else {
-					int positon = 0;
-					for (d = 0; d < union_length;d++) {
-						if (strcmp(T[b][c],a[d])==0) {
-							positon++;
-							break;
-							//printf("postion is %d\n",positon);
-						}
-						else {
-							positon++;
-						}
-					}
-					// printf("url is %s, postion oid is %d,now is %d\n",T[b][c],c,positon);
-					total += fabs((double)(c+1)/mylength-(double)positon/union_length);
-				}
-			}
-		}
-		// printf("toatal is %f\n",total);
-		if (total < mini) {
-			mini = total;
-		}
+static void printPrefs(elem *prefs, int nmemb) {
 
-	}
-	else
-	{
-		for (i = l; i <= r; i++)
-		{
-			swap(a[l], a[i]);
-			permute(a, l + 1, r);
-			swap(a[l], a[i]); //backtrack
-		}
-	}
+    for (int d = 0; d < nmemb; d++) {
+        printf("%s, ", prefs[d].name);
+        
+        for (int p = 0; p < nmemb; p++) {
+            printf("%.4f ", prefs[d].places[p]);
+        }
+        printf("\n");
+    }
 }
-
-void permute2(char **a, int l, int r)
-{
-	int i, j;
-	int b, c, d;
-	if (l == r) {
-		// base case
-		// start calculation
-		double total = 0;
-		
-		for (b = 0; b < T_number; b++) {
-			int mylength = 0;
-			for (c = 0; c < max_url; c++) {
-				if (strcmp(T[b][c], "") == 0) {
-					;
-				}
-				else {
-					mylength++;
-				}
-			}
-			// printf("mylenght is %d\n", mylength);
-			for (c = 0; c < max_url; c++) {
-				if (strcmp(T[b][c], "") == 0) {
-					;
-				}
-				else {
-					int positon = 0;
-					for (d = 0; d < union_length; d++) {
-						if (strcmp(T[b][c], a[d]) == 0) {
-							positon++;
-							break;
-							//printf("postion is %d\n",positon);
-						}
-						else {
-							positon++;
-						}
-					}
-					// printf("url is %s, postion oid is %d,now is %d\n",T[b][c],c,positon);
-					total += fabs((double)(c + 1) / mylength - (double)positon / union_length);
-				}
-			}
-		}
-
-		/*
-		for (j = 0; j <= r; j++) {
-			printf("%s  ", a[j]);
-		}
-		printf("\n");
-		printf("toatal is %f\n",total);
-		*/
-	
-		if (fabs(total-mini)<0.01) {
-		    printf("%f\n",total);
-			// printf("result\n");
-			for (j = 0; j <= r; j++) {
-				printf("%s\n", a[j]);
-			}
-			printf("\n");
-		}
-
-
-	}
-	else
-	{
-		for (i = l; i <= r; i++)
-		{
-			swap(a[l], a[i]);
-			permute2(a, l + 1, r);
-			swap(a[l], a[i]); //backtrack
-		}
-	}
-}
-
-
-
 
 int main(int argc, char *argv[]) {
-	int i, j, k;
-	char str1[URL_length];
-	int max_length = -999;
-	for (i = 1; i < argc;i++) {
-		int cnt = 0;
-		char base[path_length] = "";
-		strcat(base, argv[i]);
-		// printf("%s\n",base);
-		FILE *ptr = fopen(base,"r");
-		if (ptr == NULL) {
-			printf("Failed on open file\n");
-			return 1;
-		}
-		while(fscanf(ptr,"%s",str1)==1){
-			//printf("%s   ", str1);
-			cnt++;
-		}
-		//printf("\n");
-		fclose(ptr);
 
-		char **myarray = malloc(cnt * sizeof(char *));
-		
-		cnt = 0;
-		FILE *ptr2 = fopen(base, "r");
-		while (fscanf(ptr2, "%s", str1) == 1) {
-			myarray[cnt] = malloc(URL_length * sizeof(char));
-			strcpy(myarray[cnt], str1);
-			cnt++;
-		}
-		fclose(ptr2);
-		if (cnt>max_length) {
-			max_length = cnt;
-		}
-		for (j = 0; j < cnt; j++) {
-			// printf("%s\n", myarray[j]);
-		}
-		
-		
-	}
-	// printf("max is %d\n",max_length);
-	// printf("agrc is %d\n",argc);
-	max_url = max_length;
-	T_number = argc-1;
-	T = malloc((argc-1) * sizeof(char **));
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s {rank list}\n", argv[0]);
+        exit(1);
+    }
 
-	for (i = 0; i < argc-1; i++) {
-		T[i] = malloc(max_length * sizeof(char*));
-		for (j = 0; j < max_length; j++) {
-			T[i][j] = malloc(URL_length * sizeof(char));
-			for (k = 0; k < URL_length;k++) {
-				T[i][j][k] = 0;
-			}
-		}
-	}
-	// defuck no idea what going on, but the bug gone for no reason
-	for (i = 1; i < argc; i++) {
-		int cnt = 0;
-		char base2[path_length] = "";
-		strcat(base2, argv[i]);
-		// printf("%s\n",base);
-		FILE *ptr = fopen(base2, "r");
-		if (ptr == NULL) {
-			printf("Failed on open file\n");
-			return 1;
-		}
-		while (fscanf(ptr, "%s", str1) == 1) {
-			//printf("%s   ", str1);
-			cnt++;
-		}
-		fclose(ptr);
+    int nRanks = argc-1;
 
-		char **myarray = malloc(cnt * sizeof(char *));
+    StrDict *ranks = calloc(sizeof(StrDict), argc-1); //page-position dictionary (T_i corresponds to rank[i-1])
+    
+    for (int i = 0; i < nRanks; i++) {
+        ranks[i] = newStrDict();
+    }
 
-		cnt = 0;
-		FILE *ptr2 = fopen(base2, "r");
-		while (fscanf(ptr2, "%s", str1) == 1) {
-			myarray[cnt] = malloc(URL_length * sizeof(char));
-			strcpy(T[i-1][cnt], str1);
-			cnt++;
-		}
-		fclose(ptr2);
-	}
-	// test 3d
-	/*
-	for (i = 0; i < 2;i++) {
-		for (j = 0; j < max_length;j++) {
-			printf("%s ",T[i][j]);
-		}
-		printf("\n");
-	}
-	*/
-	int word_cnt = 0;
-	char **myunion = malloc(30*sizeof(char *));
-	for (i = 0; i < 30;i++) {
-		myunion[i] = malloc(sizeof(char)*URL_length);
-		for (j = 0; j < URL_length;j++) {
-			myunion[i][j] = 0;
-		}
-	}
+    SList pages = newSList();
+    int file = 1;
+    while (file <= nRanks) {   //for each rank file
 
-	for (j = 0; j < max_length; j++) {
-		if (strcmp(T[0][j],"")==0) {
-			;
-		}
-		else {
-			strcpy(myunion[word_cnt], T[0][j]);
-			word_cnt++;
-		}
-	}
-	for (i = 1; i < 2; i++) {
-		for (j = 0; j < max_length; j++) {
-			// T[i][j] is a url
-			int found = 0;
-			for (k = 0; k < word_cnt;k++) {
-				if (strcmp(T[i][j],"") == 0) {
-					// empty string assume found
-					found = 1;
-				}
-				if (strcmp(T[i][j], myunion[k]) == 0) {
-					found = 1;
-				}
-			}
-			if (found == 0) {
-				strcpy(myunion[word_cnt],T[i][j]);
-				printf("not found is %s,i %d, j %d\n", T[i][j],i,j);
-				word_cnt++;
-			}
-		}
-	}
-	// test union
-	// printf("word_cnt is %d\n",word_cnt);
-	/*
-	for (i = 0; i < word_cnt;i++) {
-		printf("myuni is %s\n",myunion[i]);
-	}
-	*/
-	union_length = word_cnt;
-	// printf("start permutation\n");
-	permute(myunion, 0, --word_cnt);
-	// printf("%f\n",mini);
-	permute2(myunion, 0, word_cnt);
-	return 0;
+        FILE *rankf = fopen(argv[file], "r");
+        char lineBuf[512] = {0};
+        int line = 1;
+        //printf("opening %s\n", argv[file]);
+        while (lineBuf == fgets(lineBuf, 512, rankf)) {    //for each page in the rank
+        
+            lineBuf[strlen(lineBuf)-1] = '\0';      //strip newline
+            pages = addStr(pages, lineBuf);
+            ranks[file-1] = addIndex(ranks[file-1], line, lineBuf);
+            line++;
+        }
+        fclose(rankf);
+        file++;
+    }
+    
+    int nPages = nStr(pages);
+    elem *prefs = calloc(sizeof(elem), nPages);    //array of each page and their pos preferences
+    if (prefs == NULL) abort();
+    
+    for (int a = 0; a < nPages; a++) {
+        
+        pages = popFirstStr(pages, &(prefs[a].name));    //add name to preference
+        prefs[a].places = calloc(sizeof(int), nPages);
+        if (prefs[a].places == NULL) abort();
+    }
+    //debugging
+    //printf("%d, %d\n", nEntries(ranks[0]),nEntries(ranks[1]));
+    
+    //PREFERENCE: position where W(c, p) is minimal
+    /*we now have our table:
+    url#    url#    url#
+    ...     ...     ...
+    ...     ...     ...
+    */
+    //we will now iterate through each url from 0 to nPages-1, and add its preferences
+    
+   for (int i = 0; i < nPages; i++) {
+        
+        for (int p = 0; p < nPages; p++) {
+            
+            prefs[i].places[p] = scaledFoot(prefs[i].name, p+1, ranks, nRanks, nPages);
+            prefs[i].nPrefs++;
+        }
+    }
+    
+    printPrefs(prefs, nPages);
+    
+    
+    
+    for (int i = 0; i < nRanks; i++)    //clean up
+        freeStrDict(ranks[i]);
+    
+    for (int d = 0; d < nPages; d++) {
+        free(prefs[d].name);
+        free(prefs[d].places);
+    }
+    free(prefs);
+    free(ranks);
+        
+    return EXIT_SUCCESS; 
 }
+/*
+void position(c, p){
+    double storage;
+    int sum_cp[50] = 0;
+    int  i, a
+
+    while (i <= k) {
+        storage = fabs((c/n)-(p/n));
+        sum_cp[a] = sum_cp[a] + storage;
+         
+
+        c++;
+        i++;
+        a++;
+
+    }
+}
+*/
+
+void swap(char *str1, char *str2) {
+    char *temp = (char *)malloc((strlen(str1) + 1) * sizeof(char));
+    strcpy(temp, str1); // put str1 into temp 
+    strcpy(str1, str2); // put str 2 in str1
+    strcpy(str2, temp); // put temp (str1) into str2 
+    free(temp); // free once swapped 
+} 
+
+
+void calculation (char **a, int l, int r) {
+
+    int b, c, d, i, j; 
+    int length;
+    int max; 
+    char ***T;
+    if (l == r) {
+        // base case
+        // start calculation
+        double box = 0;
+        double mini = 999;
+
+        int e = 0;
+        while (e<= r) {
+            e++; 
+        }
+
+        int t;  
+        int b = 0;
+        while (b < t) {
+            int mylength, x = 0;
+            while (x < max) {
+                if (strcmp(T[b][c],"") == 0) {
+                    ;  
+                } else {
+                    mylength++; 
+                }
+                c++;
+            }
+            int c = 0;
+            while (c < max) {
+                if (strcmp(T[b][c], "") == 0) {
+                    ;
+                } else {
+                    int positon, d =0;
+                    while(d < length) {
+                        if (strcmp(T[b][c],a[d])==0) {
+                            positon++;
+                            break;
+                        } else {
+                            positon++;
+                        }
+                        d++;
+                    }
+                    box += fabs((double)(c+1)/mylength-(double)positon/length);
+                    // The main calculation formula 
+                }
+                c++;
+            }
+            b++;
+        }
+       
+        if (box < mini) {
+            mini = box;
+        }  
+    } else {
+        int i = l;
+        while (i <= r) {
+            swap(a[l], a[i]);
+            calculation(a, l + 1, r);
+            swap(a[l], a[i]); 
+            i++;
+        } 
+    }
+}
+
+
+
